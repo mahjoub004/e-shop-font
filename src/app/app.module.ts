bbfeb89700f132d5b9331a56a910142c07fb338c
jebalimahjoub <mahjoub_jebali@yahoo.fr>
2023-03-06 11:09:25 +0100

    route for component search , create search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { ProduitService } from './services/produit.service';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductCategoryMenuComponent
+    ProductCategoryMenuComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
