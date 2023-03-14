1ef339c6bf7fb1d7d78ce292f81e096ea32fb4c8
jebalimahjoub <mahjoub_jebali@yahoo.fr>
2023-03-14 16:58:41 +0100

    add event handler to check validation status when submit button clicked

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -102,6 +102,11 @@ export class CheckoutComponent implements OnInit {
   }
   onSubmit() {
     console.log('handling the submit buttom');
+    if (this.checkoutFormGroup.invalid) {
+        this.checkoutFormGroup.markAllAsTouched();
+        console.log('je suis dans le if de onSubmit');
+
+    }
     console.log(this.checkoutFormGroup.get('customer')?.value);
     console.log("the shipping address is " + this.checkoutFormGroup.get('customer')?.value.email);
     console.log("the shipping address country is "+ this.checkoutFormGroup.get('shippingAddress')?.value.country.name);
